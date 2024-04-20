1a006b5de4d5c3b44a342b66ddd044b80a362e41
Alexsandro Soares Teixeira <notbooksoares@gmail.com>
2024-04-20 17:20:10 -0300

    incrementando funcionalidades

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,20 +1,36 @@
 import style from './Card.module.css'
 import ButtonB from './ButtonB'
+import { useState } from 'react';
 
 
 function Card({ img, title, tech, description, repo, site }) {
+
+    const [info, setInfo] = useState(false);
+
+    function InfoOn(){
+        setInfo(true);
+    }
+
+    function InfoOf(){
+        setInfo(false);
+    }
+
     return (
-        <div className={style.card}>
-            <a href={site}>
+        <div onMouseLeave={InfoOf} className={style.card}>
+            <a onMouseEnter={InfoOn} href={site}>
                 <img src={img} alt='project' />
             </a>
 
-            <section>
-                <h3>{title}</h3>
-                <p><strong>Tecnologia: </strong>{tech}</p>
-                <p>{description}</p>
-                <ButtonB text={'Acesse o repositório'} link={repo} />
-            </section>
+            {info === true && (
+                <section>
+                    <h3>{title}</h3>
+                    <p><strong>Tecnologia: </strong>{tech}</p>
+                    <p>{description}</p>
+                    <ButtonB text={'Acesse o repositório'} link={repo} />
+                </section>
+            )}
+
+
         </div>
     )
 }
--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,21 +1,60 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
-import ButtonB from '../elements/ButtonB'
+import { useEffect, useState } from "react"
+
 
 function Presentation() {
+
+    const [text, setText] = useState("");
+    const toRotate = ["Alexsandro Soares", "Desenvolvedor Front End", "Product Manager"];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, SetIsDeleting] = useState(false);
+    const period = 150;
+    const [delta, setDelta] = useState(100);
+
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            toType()
+        }, delta);
+        return () => {
+            clearInterval(ticker)
+        }
+    }, [text])
+
+    const toType = () => {
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+            SetIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText == ""){
+            SetIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+
+        }
+
+    }
+
+
     return (
         <div id='Presentation' className={styles.Presentation}>
             <h3><strong>Bem-vindo ao meu Portfólio</strong></h3>
-            <h1>Olá, eu sou Juan Reis!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>
-                Sou um apaixonado por tecnologia e soluções inovadoras. <br/>
-                Como Product Manager, eu tenho o compromisso de resolver problemas <br/>
-                complexos e trazer resultados excepcionais para os negócios. <br/>
-                Meus projetos já geraram milhões de reais em receita anual <br/>
-                estou sempre em busca de novos desafios para superar. <br/>
+                Sou um apaixonado por tecnologia e soluções inovadoras. <br />
+                Como Product Manager, eu tenho o compromisso de resolver problemas <br />
+                complexos e trazer resultados excepcionais para os negócios. <br />
+                Meus projetos já geraram milhões de reais em receita anual <br />
+                estou sempre em busca de novos desafios para superar. <br />
             </p>
 
-            <ButtonA link={'https://github.com/alesantos102'} text={'Conecte-se comigo!'}/>
+            <ButtonA link={'https://github.com/alesantos102'} text={'Conecte-se comigo!'} />
         </div>
     )
 }
